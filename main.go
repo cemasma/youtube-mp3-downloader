a79eaff391b5e0fcd648bc1face5348e133289e7
Cem Asma <dev.cemas@gmail.com>
2018-10-20 23:46:16 +0300

    name update

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 )
 
 func main() {
-	url := flag.String("url", "", "You must send it with youtube url.\nExample: ymp3d --url https://www.youtube.com/watch?v=t1dXdsUricU")
+	url := flag.String("url", "", "You must send it with youtube url.\nExample: youtube-mp3-downloader --url https://www.youtube.com/watch?v=t1dXdsUricU")
 	playlist := flag.Bool("playlist", false, "If you want to download all of the videos from a playlist you should use this.\n"+
-		"Example: ymp3d --url https://www.youtube.com/watch?v=80214E8FuBo&list=RDMM80214E8FuBo&start_radio=1 --playlist true")
+		"Example: youtube-mp3-downloader --url https://www.youtube.com/watch?v=80214E8FuBo&list=RDMM80214E8FuBo&start_radio=1 --playlist true")
 	flag.Parse()
 
 	if len(*url) > 0 {
